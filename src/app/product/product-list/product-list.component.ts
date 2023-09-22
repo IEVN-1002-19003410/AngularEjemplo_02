8d3bd3d78a0a4a8d3747bf406e22bd284e997984
Ismael Rangel <ismaelrangel994@gmail.com>
2023-09-21 18:53:02 -0600

    21-09-2023

---
+++ src/app/product/product-list/product-list.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+
+  imageWidth:number=50;
+  imageMargin:number=2;
+  muestraImg:boolean=true;
+  listFilter:string='';
+
+  productos:any[]=[
+    {
+      "productoId":1,
+      "Modelo":"Sentra",
+      "Descripcion":"4 puertas familiar",
+      "year":"febrero 3 2020",
+      "precio":120000,
+      "Marca":"Nissan",
+      "Color":"Verde",
+      "imagenUrl":"https://upload.wikimedia.org/wikipedia/commons/9/92/Nuevo_Nissan_Sentra_2023.webp"
+    },
+    {
+      "productoId":2,
+      "Modelo":"A4",
+      "Descripcion":"2 puertas",
+      "year":"marzo 5 2022",
+      "precio":180000,
+      "Marca":"Audi",
+      "Color":"Blanco",
+      "imagenUrl":"https://upload.wikimedia.org/wikipedia/commons/9/9b/Audi_A4_B9_Limousine_3.0_TDI_quattro.JPG"
+    },
+    {
+      "productoId":3,
+      "Modelo":"Rio",
+      "Descripcion":"2 puertas familiar",
+      "year": "enero 2 2023",
+      "precio":135000,
+      "Marca":"Kia",
+      "Color":"Azul",
+      "imagenUrl":"https://upload.wikimedia.org/wikipedia/commons/6/60/2018_Kia_Rio_EX_-_Side.jpg"
+    }
+  ]
+
+}
